288e72f6862eb29404dcbc076e80612d44ec241e
sueyoshi <sueyoshi@sueyoshi-VirtualBox.(none)>
2014-03-20 11:05:38 +0900

    users routes add

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 WhatApp::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+  resources :users
+      	# The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
